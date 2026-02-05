934439147d0c393d16088e12dda1f7691eb1315e
natsu <natsu@USERnoMacBook-Air.local>
2026-02-05 12:18:14 +0900

    add: GreenCardヘッダー追加

--- components/greens/GreenCard.tsx
+++ components/greens/GreenCard.tsx
@@ -6,7 +6,10 @@ interface GreenCardProps {
 
 export default function GreenCard({ hole }: GreenCardProps) {
   return (
-    <div>
+    <div className="w-[600]">
+      <div className="h-10 bg-gray-800 text-white font-bold text-center flex items-center justify-center">
+        Hole {hole}
+      </div>
       <GreenCanvas hole={hole} />
     </div>
   );
