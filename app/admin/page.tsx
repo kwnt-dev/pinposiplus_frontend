5838f0ac5c746fdd37912a8811fb622feb0b7e65
natsu <natsu@USERnoMacBook-Air.local>
2026-02-13 19:01:37 +0900

    add: ダッシュボードページの左右パネルレイアウト作成

---
+++ app/admin/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>ダッシュボード</h1>
+      <div className="flex gap-4">
+        <div className="flex-1"> 左</div>
+        <div className="flex-1"> 右</div>
+      </div>
+    </div>
+  );
+}
